a5bbf52de00393568edb3430275cc37219dc5ebe
shyamagu <shyamagu@outlook.com>
2022-06-08 21:54:33 +0900

    enable to select detection model(1 or 3)

--- Model/Face/FaceService.cs
+++ Model/Face/FaceService.cs
@@ -11,88 +11,106 @@ using SixLabors.ImageSharp.Formats.Jpeg;
 
 public class FaceService {
 
-    const string RECOGNITION_MODEL4 = RecognitionModel.Recognition04;
-
     public List<FaceResultModel> FaceResult = new();
 
     public IFaceClient GetFaceClient(string key,string endpoint){
         return new FaceClient(new ApiKeyServiceClientCredentials(key)) { Endpoint = endpoint };
     }
 
-    public async Task<IList<DetectedFace>> DetectFaceExtract(IFaceClient client, Stream imageStream){
-        return await client.Face.DetectWithStreamAsync(imageStream,
-            returnFaceLandmarks: true,
-            returnFaceAttributes: new List<FaceAttributeType> { FaceAttributeType.Accessories, FaceAttributeType.Age,
-            FaceAttributeType.Blur, FaceAttributeType.Emotion, FaceAttributeType.Exposure, FaceAttributeType.FacialHair,
-            FaceAttributeType.Glasses, FaceAttributeType.Hair, FaceAttributeType.HeadPose,
-            FaceAttributeType.Makeup, FaceAttributeType.Noise, FaceAttributeType.Occlusion, FaceAttributeType.Smile},
-            // FaceAttributeType.QualityForRecognition is missing
-            // We specify detection model 1 because we are retrieving attributes.
-            detectionModel: DetectionModel.Detection01,
-            recognitionModel: RECOGNITION_MODEL4);
+    public async Task<IList<DetectedFace>> DetectFaceExtract(IFaceClient client, Stream imageStream, string modelVersion){
+
+        if(modelVersion=="1"){
+            return await client.Face.DetectWithStreamAsync(imageStream,
+                returnFaceLandmarks: true,
+                returnFaceAttributes: new List<FaceAttributeType> { FaceAttributeType.Accessories, FaceAttributeType.Age,
+                FaceAttributeType.Blur, FaceAttributeType.Emotion, FaceAttributeType.Exposure, FaceAttributeType.FacialHair,
+                FaceAttributeType.Glasses, FaceAttributeType.Hair, FaceAttributeType.HeadPose,
+                FaceAttributeType.Makeup, FaceAttributeType.Noise, FaceAttributeType.Occlusion, FaceAttributeType.Smile},
+                // FaceAttributeType.QualityForRecognition is missing
+                // We specify detection model 1 because we are retrieving attributes.
+                detectionModel: DetectionModel.Detection01,
+                recognitionModel: RecognitionModel.Recognition04);
+        //ModelVersion=="3"
+        }else{
+            return await client.Face.DetectWithStreamAsync(imageStream,
+                returnFaceLandmarks: true,
+                returnFaceAttributes: new List<FaceAttributeType> { FaceAttributeType.HeadPose, FaceAttributeType.Mask },
+                detectionModel: DetectionModel.Detection03,
+                recognitionModel: RecognitionModel.Recognition04);
+
+        }
     }
 
-    public List<string> GetFaceAnalysisResult(DetectedFace face){
+    public List<string> GetFaceAnalysisResult(DetectedFace face, string modelVersion){
 
         List<string> faceApiResult = new();
 
-        faceApiResult.Add($"Age : {face.FaceAttributes.Age}");
-        faceApiResult.Add($"Blur : {face.FaceAttributes.Blur.BlurLevel}");
-        
-        string emotionType = string.Empty;
-        double emotionValue = 0.0;
-        Emotion emotion = face.FaceAttributes.Emotion;
-        if (emotion.Anger > emotionValue) { emotionValue = emotion.Anger; emotionType = "Anger"; }
-        if (emotion.Contempt > emotionValue) { emotionValue = emotion.Contempt; emotionType = "Contempt"; }
-        if (emotion.Disgust > emotionValue) { emotionValue = emotion.Disgust; emotionType = "Disgust"; }
-        if (emotion.Fear > emotionValue) { emotionValue = emotion.Fear; emotionType = "Fear"; }
-        if (emotion.Happiness > emotionValue) { emotionValue = emotion.Happiness; emotionType = "Happiness"; }
-        if (emotion.Neutral > emotionValue) { emotionValue = emotion.Neutral; emotionType = "Neutral"; }
-        if (emotion.Sadness > emotionValue) { emotionValue = emotion.Sadness; emotionType = "Sadness"; }
-        if (emotion.Surprise > emotionValue) { emotionType = "Surprise"; }
-        faceApiResult.Add($"Main Emotion : {emotionType}");
+        if(modelVersion=="1"){
+            faceApiResult.Add($"Age : {face.FaceAttributes.Age}");
+            faceApiResult.Add($"Blur : {face.FaceAttributes.Blur.BlurLevel}");
+            
+            string emotionType = string.Empty;
+            double emotionValue = 0.0;
+            Emotion emotion = face.FaceAttributes.Emotion;
+            if (emotion.Anger > emotionValue) { emotionValue = emotion.Anger; emotionType = "Anger"; }
+            if (emotion.Contempt > emotionValue) { emotionValue = emotion.Contempt; emotionType = "Contempt"; }
+            if (emotion.Disgust > emotionValue) { emotionValue = emotion.Disgust; emotionType = "Disgust"; }
+            if (emotion.Fear > emotionValue) { emotionValue = emotion.Fear; emotionType = "Fear"; }
+            if (emotion.Happiness > emotionValue) { emotionValue = emotion.Happiness; emotionType = "Happiness"; }
+            if (emotion.Neutral > emotionValue) { emotionValue = emotion.Neutral; emotionType = "Neutral"; }
+            if (emotion.Sadness > emotionValue) { emotionValue = emotion.Sadness; emotionType = "Sadness"; }
+            if (emotion.Surprise > emotionValue) { emotionType = "Surprise"; }
+            faceApiResult.Add($"Main Emotion : {emotionType}");
 
-        faceApiResult.Add(
-            $"HeadPose : {string.Format("Pitch: {0}, Roll: {1}, Yaw: {2}", Math.Round(face.FaceAttributes.HeadPose.Pitch, 2), Math.Round(face.FaceAttributes.HeadPose.Roll, 2), Math.Round(face.FaceAttributes.HeadPose.Yaw, 2))}"
-        );
+            faceApiResult.Add(
+                $"HeadPose : {string.Format("Pitch: {0}, Roll: {1}, Yaw: {2}", Math.Round(face.FaceAttributes.HeadPose.Pitch, 2), Math.Round(face.FaceAttributes.HeadPose.Roll, 2), Math.Round(face.FaceAttributes.HeadPose.Yaw, 2))}"
+            );
 
-        faceApiResult.Add($"Exposure : {face.FaceAttributes.Exposure.ExposureLevel}");
-        faceApiResult.Add($"FacialHair : {string.Format("{0}", face.FaceAttributes.FacialHair.Moustache + face.FaceAttributes.FacialHair.Beard + face.FaceAttributes.FacialHair.Sideburns > 0 ? "Yes" : "No")}");
-        faceApiResult.Add($"Glasses : {face.FaceAttributes.Glasses}");
+            faceApiResult.Add($"Exposure : {face.FaceAttributes.Exposure.ExposureLevel}");
+            faceApiResult.Add($"FacialHair : {string.Format("{0}", face.FaceAttributes.FacialHair.Moustache + face.FaceAttributes.FacialHair.Beard + face.FaceAttributes.FacialHair.Sideburns > 0 ? "Yes" : "No")}");
+            faceApiResult.Add($"Glasses : {face.FaceAttributes.Glasses}");
 
-        // Get hair color
-        Hair hair = face.FaceAttributes.Hair;
-        string? color = null;
-        if (hair.HairColor.Count == 0) { if (hair.Invisible) { color = "Invisible"; } else { color = "Bald"; } }
-        HairColorType returnColor = HairColorType.Unknown;
-        double maxConfidence = 0.0f;
-        foreach (HairColor hairColor in hair.HairColor)
-        {
-            if (hairColor.Confidence <= maxConfidence) { continue; }
-            maxConfidence = hairColor.Confidence; returnColor = hairColor.Color; color = returnColor.ToString();
+            // Get hair color
+            Hair hair = face.FaceAttributes.Hair;
+            string? color = null;
+            if (hair.HairColor.Count == 0) { if (hair.Invisible) { color = "Invisible"; } else { color = "Bald"; } }
+            HairColorType returnColor = HairColorType.Unknown;
+            double maxConfidence = 0.0f;
+            foreach (HairColor hairColor in hair.HairColor)
+            {
+                if (hairColor.Confidence <= maxConfidence) { continue; }
+                maxConfidence = hairColor.Confidence; returnColor = hairColor.Color; color = returnColor.ToString();
+            }
+            faceApiResult.Add($"Hair : {color}");
+            faceApiResult.Add($"Bald : {face.FaceAttributes.Hair.Bald}");
+            faceApiResult.Add($"Invisible : {face.FaceAttributes.Hair.Invisible}");
+
+            // Get accessories of the faces
+            List<Accessory> accessoriesList = (List<Accessory>)face.FaceAttributes.Accessories;
+            int count = face.FaceAttributes.Accessories.Count;
+            string accessory; string[] accessoryArray = new string[count];
+            if (count == 0) { accessory = "NoAccessories"; }
+            else
+            {
+                for (int i = 0; i < count; ++i) { accessoryArray[i] = accessoriesList[i].Type.ToString(); }
+                accessory = string.Join(",", accessoryArray);
+            }
+            faceApiResult.Add($"Accessories : {accessory}");
+
+            // Get more attributes
+            faceApiResult.Add($"Makeup : {string.Format("{0}", (face.FaceAttributes.Makeup.EyeMakeup || face.FaceAttributes.Makeup.LipMakeup) ? "Yes" : "No")}");
+            faceApiResult.Add($"Noise : {face.FaceAttributes.Noise.NoiseLevel}");
+            faceApiResult.Add($"Occlusion : {string.Format("EyeOccluded: {0}", face.FaceAttributes.Occlusion.EyeOccluded ? "Yes" : "No")} " +
+                $" {string.Format("ForeheadOccluded: {0}", face.FaceAttributes.Occlusion.ForeheadOccluded ? "Yes" : "No")}   {string.Format("MouthOccluded: {0}", face.FaceAttributes.Occlusion.MouthOccluded ? "Yes" : "No")}");
+            faceApiResult.Add($"Smile : {face.FaceAttributes.Smile}");
         }
-        faceApiResult.Add($"Hair : {color}");
-        faceApiResult.Add($"Bald : {face.FaceAttributes.Hair.Bald}");
-        faceApiResult.Add($"Invisible : {face.FaceAttributes.Hair.Invisible}");
 
-        // Get accessories of the faces
-        List<Accessory> accessoriesList = (List<Accessory>)face.FaceAttributes.Accessories;
-        int count = face.FaceAttributes.Accessories.Count;
-        string accessory; string[] accessoryArray = new string[count];
-        if (count == 0) { accessory = "NoAccessories"; }
-        else
-        {
-            for (int i = 0; i < count; ++i) { accessoryArray[i] = accessoriesList[i].Type.ToString(); }
-            accessory = string.Join(",", accessoryArray);
+        if(modelVersion=="3"){
+            faceApiResult.Add(
+                $"HeadPose : {string.Format("Pitch: {0}, Roll: {1}, Yaw: {2}", Math.Round(face.FaceAttributes.HeadPose.Pitch, 2), Math.Round(face.FaceAttributes.HeadPose.Roll, 2), Math.Round(face.FaceAttributes.HeadPose.Yaw, 2))}"
+            );
+            faceApiResult.Add($"Mask : {face.FaceAttributes.Mask.NoseAndMouthCovered}");
         }
-        faceApiResult.Add($"Accessories : {accessory}");
-
-        // Get more attributes
-        faceApiResult.Add($"Makeup : {string.Format("{0}", (face.FaceAttributes.Makeup.EyeMakeup || face.FaceAttributes.Makeup.LipMakeup) ? "Yes" : "No")}");
-        faceApiResult.Add($"Noise : {face.FaceAttributes.Noise.NoiseLevel}");
-        faceApiResult.Add($"Occlusion : {string.Format("EyeOccluded: {0}", face.FaceAttributes.Occlusion.EyeOccluded ? "Yes" : "No")} " +
-            $" {string.Format("ForeheadOccluded: {0}", face.FaceAttributes.Occlusion.ForeheadOccluded ? "Yes" : "No")}   {string.Format("MouthOccluded: {0}", face.FaceAttributes.Occlusion.MouthOccluded ? "Yes" : "No")}");
-        faceApiResult.Add($"Smile : {face.FaceAttributes.Smile}");
 
         return faceApiResult;
     }
